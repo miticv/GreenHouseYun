76084437a732aca1f3f9f3b4896737dbc725e3db
Vladimir Mitic <miticv@gmail.com>
2015-07-28 15:01:34 -0400

    Creating build folder

--- GreenHouseWeb/src/client/app/scripts/routing.js
+++ GreenHouseWeb/src/client/app/scripts/routing.js
@@ -3,6 +3,6 @@
 angular.module('greenhouse', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'cgBusy', 'angularMoment', 'ngMessages', 'chart.js']).
   config(['$routeProvider', function ($routeProvider) {
         $routeProvider.
-         when('/', { templateUrl: '/app/views/home.html', controller: 'homeController' }).
+         when('/', { templateUrl: 'app/views/home.html', controller: 'homeController' }).
          otherwise({ redirectTo: '/' });
     }]);
